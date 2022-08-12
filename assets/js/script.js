ce68b0608c25c8894c3d9b2b07fe079cbef63211
Jinun20th <phancongthanh23071999@gmail.com>
2022-08-12 22:11:59 +0700

    Last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,72 +2,32 @@ const sections = document.querySelectorAll(".team-section");
 const panes = document.querySelectorAll(".team-panel");
 
 sections.forEach((tabs, index) => {
-    const pane = panes[index];
-    tabs.onclick = function () {
-        document.querySelector(".team-section.active").classList.remove('active');
-        document.querySelector(".team-panel.active").classList.remove('active');
-        this.classList.add('active');
-        pane.classList.add('active');
-        /* more.classList.remove('active');
-        less.classList.remove('active');
-        const members = document.querySelectorAll(".team-panel.active .member-container");
-        if (members.length > 8) {
-            more.classList.add('active');
-        }
-        hideMembers(); */
-    }
+  const pane = panes[index];
+  tabs.onclick = function () {
+    document.querySelector(".team-section.active").classList.remove('active');
+    document.querySelector(".team-panel.active").classList.remove('active');
+    this.classList.add('active');
+    pane.classList.add('active');
+  }
 }
 )
 
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-/* const more = document.querySelector("#more");
-const less = document.querySelector("#less");
-
-const hideMembers = () => {
-    const members = document.querySelectorAll(".team-panel.active .member-container");
-    if (members.length > 8) {
-        for (var i = 0; i < members.length; i++) {
-            if (i > 7) {
-                members[i].classList.add('hide');
-            }
-        }
-    }
-}
-
-const showMembers = () => {
-    const members = document.querySelectorAll(".team-panel.active .member-container.hide");
-    for (var i = 0; i < members.length; i++) {
-        members[i].classList.remove('hide');
-    }
-}
-
-more.onclick = function () {
-    this.classList.remove('active');
-    less.classList.add('active');
-    showMembers();
-}
-
-less.onclick = function () {
-    this.classList.remove('active');
-    more.classList.add('active');
-    hideMembers();
-} */
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 // swiper libray initialization
 new Swiper('.swiper-container', {
-    loop: true,
-    slidesPerView: 5,
-    autoplay: {
-        delay: 3000
-    },
+  loop: true,
+  slidesPerView: 5,
+  autoplay: {
+    delay: 3000
+  },
 })
