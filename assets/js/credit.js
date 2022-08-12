8c2889cdd7f16bde4ccd418b401358c4447a1e37
Jinun20th <phancongthanh23071999@gmail.com>
2022-08-12 22:48:25 +0700

    Last last commit

---
+++ assets/js/credit.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll(".team-section");
+const panes = document.querySelectorAll(".team-panel");
+
+sections.forEach((tabs, index) => {
+  const pane = panes[index];
+  tabs.onclick = function () {
+    document.querySelector(".team-section.active").classList.remove('active');
+    document.querySelector(".team-panel.active").classList.remove('active');
+    this.classList.add('active');
+    pane.classList.add('active');
+  }
+}
+)
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
